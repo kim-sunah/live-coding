a7d7559884c4eb112f83981052da4519c923bf8b
kim-sunah <69501036+kim-sunah@users.noreply.github.com>
2025-01-13 10:37:28 +0900

    Add files via upload

---
+++ string.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<body>
+    <h1>String & String Operator</h1>
+    <?php echo
+    "hello \"w\" ord";
+    ?>
+    <h2>concatenation Operator</h2>
+    <php 
+    echo "Hello " . "World!";
+    ?>
+</body>
+</html>
