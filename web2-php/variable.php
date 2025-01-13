04d226d287b7420c94bf798aa61c1364c43b66c7
kim-sunah <1004_sunah@naver.com>
2025-01-13 12:05:42 +0900

    php의 변수

---
+++ web2-php/variable.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+        <h1>
+            Variable
+        </h1>
+        <p> <?php
+        $a = 11;
+        echo 
+        $a+1;
+        ?></p>
+       <p>
+       <?php
+       $name = "suna";
+        echo "Lorem ipsum dolor ".$name." sit amet consectetur adipisicing sun elit. ".$name." Expedita quisquam ".$name." laudantium quos, explsunaicabo quas, atque nulla autem nisi suna impedit inventore dolor accusamus eos debitis numqsunah uam odio non voluptate recusandae volupta sunah tem."
+    ?>
+       </p>
+        
+        </body>
+</html>
