b33431ad711286e4d80b401bbc031addb5ea5611
kim-sunah <1004_sunah@naver.com>
2025-01-13 13:42:53 +0900

    url 파라미터

---
+++ web2-php/parameter.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    </head>
+    <body>
+        안녕하세요
+        <?php
+        echo $_GET['name'];
+        ?>님
+    </body>
+</html>
